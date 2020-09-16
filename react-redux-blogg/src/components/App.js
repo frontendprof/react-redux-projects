dc0b8e235a32fe222c1815473b191b610f0d4515
frontendprof <abdumaliksharipov@gmail.com>
2020-09-16 18:37:36 +0500

    Middlewares in redux

--- react-redux-blogg/src/components/App.js
+++ react-redux-blogg/src/components/App.js
@@ -2,10 +2,12 @@
 
 import React from 'react';
 
+import PostList from "./PostList";
+
 const App=()=> {
     return (
         <div className="ui container">
-            Blog app            
+            <PostList />            
         </div>
     );
 };
---
+++ react-redux-blogg/src/components/PostList.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+
+
+class PostList extends Component {
+  
+    render() {
+        return (
+            <div>
+                POST LIST
+            </div>
+        );
+    };
+};
+
+export default PostList;
