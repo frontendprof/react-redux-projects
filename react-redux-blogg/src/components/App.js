e50597eac57a9263efabe91c824f583acf408f0d
frontendprof <abdumaliksharipov@gmail.com>
2020-09-16 11:43:04 +0500

    Initial setup of new project

---
+++ react-redux-blogg/src/components/App.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const App=()=> {
+    return (
+        <div className="ui container">
+            Blog app            
+        </div>
+    );
+};
+
+export default App;
