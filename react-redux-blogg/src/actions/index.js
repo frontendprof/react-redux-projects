e1533838cb6630e3891c63e2d7c191b7f0c16174
frontendprof <abdumaliksharipov@gmail.com>
2020-09-17 18:11:30 +0500

    Dispatching correct values and list building

--- react-redux-blogg/src/actions/index.js
+++ react-redux-blogg/src/actions/index.js
@@ -2,15 +2,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 
-export const fetchPosts = ()=>{
-
-    return async function(dispatch,getState){
+export const fetchPosts = ()=> async dispatch=>{
 
         const res=await jsonPlaceholder.get("./posts");
 
-        dispatch({type:"FETCH_POSTS",payload:res});
+        dispatch({type:"FETCH_POSTS",payload:res.data});
     };
-};
+
 
 
 
--- react-redux-blogg/src/reducers/index.js
+++ react-redux-blogg/src/reducers/index.js
@@ -1,7 +1,9 @@
 
 import { combineReducers } from "redux";
 
+import postsReducer from "./postsReducer";
+
 
 export default combineReducers({
-    dummy:()=>"dummy data"
-})
+    posts:postsReducer
+});
