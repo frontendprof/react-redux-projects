5ba290463d1d058d92d0f235d99fb7932fd582ac
frontendprof <abdumaliksharipov@gmail.com>
2020-09-17 18:37:35 +0500

    Displaying the user header

--- react-redux-blogg/src/components/PostList.js
+++ react-redux-blogg/src/components/PostList.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 
 import {fetchPosts} from '../actions';
+import UserHeader from "./UserHeader";
 
 
 
@@ -26,6 +27,7 @@ class PostList extends Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId}/>
                     </div>
                 </div>
 
